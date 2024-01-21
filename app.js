dace97cb224b21a832bdf18a3671e6b0a49cf05f
hlin <gywjd5126@gmail.com>
2024-01-21 23:41:03 +0900

    Toggle Side Menu

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ const menu = document.querySelector(".menu");
 const overlay = document.querySelector(".overlay");
 
 // Right menu all
-const menu__all__ul = document.querySelector(".menu__all__ul");
-const menuFurniture = document.querySelector(".menu__all__furniture");
+const allList = document.querySelectorAll(".menu__all__list");
 
 function onToggleMenu(target) {
   const other = target === projectsMenu ? allMenu : projectsMenu;
@@ -24,18 +23,66 @@ function onToggleMenu(target) {
     : overlay.classList.remove("hidden");
 }
 
+const toggleClass = (target) => target.classList.toggle("hidden");
+
 function onMouseOverMenu(e) {
-  const target = e.target.dataset.type;
-  console.log(target);
-  if (target === "furniture") {
-    menuFurniture.classList.toggle("hidden");
+  const target = e.target;
+  const type = target.dataset.type;
+  const classList = target.classList[0];
+
+  if (type) return;
+
+  switch (classList) {
+    case "menu__all__list": {
+      return toggleClass(target.lastElementChild);
+    }
+    case "menu__all__a": {
+      return toggleClass(target.nextElementSibling);
+    }
+    case "menu__all__hover": {
+      return toggleClass(target);
+    }
+    case "hover__list": {
+      return toggleClass(target.parentElement);
+    }
+    case "hover__list__heading": {
+      const ul = target.parentElement.parentElement;
+      return toggleClass(ul);
+    }
+    case "hover__list__contents": {
+      const ul = target.parentElement.parentElement;
+      return toggleClass(ul);
+    }
+    default:
+      break;
   }
+}
+
+const getImgData = async () => {
+  const accessKey = "6IrhqT0gfrZKjzbQcomAW-hOUciJchXSXsThXZLkeCw";
+  const page = "30";
+
+  const apiUrl = `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=${page}`;
+
+  await fetch(apiUrl)
+    .then((res) => res.json())
+    .then((json) => printCardList(json))
+    .catch((error) => console.log(error));
+};
+
+function printCardList(data) {
+  console.log(data);
 }
 
 function eventHandler() {
   projectsBtn.addEventListener("click", () => onToggleMenu(projectsMenu));
   allBtn.addEventListener("click", () => onToggleMenu(allMenu));
-  menu__all__ul.addEventListener("mouseover", onMouseOverMenu);
+
+  allList.forEach((list) => {
+    list.addEventListener("mouseover", onMouseOverMenu);
+    list.addEventListener("mouseout", onMouseOverMenu);
+  });
 }
 
 eventHandler();
+getImgData();
